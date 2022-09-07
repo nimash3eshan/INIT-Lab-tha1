4f0322f198653a3182b5ee3db845e5b1781d771d
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2022-09-07 16:21:36 +0530

    preventDefault()

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const inputElement = document.getElementById('form')
+inputElement.onclick = (e) => {
+	e.preventDefault()
+}
+
 function login(){
   let error = document.getElementById('error');
   error.style.display = "grid";
